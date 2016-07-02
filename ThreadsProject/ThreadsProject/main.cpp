7fbe497e17f2c583541cb29e4d1230f0021f8ec4
JiangZhongjin <Jiangzhongjin@JiangZhongjindeMacBook-Pro.local>
2016-07-02 23:52:40 +0800

    我的第一次提交，yeah

---
+++ ThreadsProject/ThreadsProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThreadsProject
+//
+//  Created by JiangZhongjin on 16/7/2.
+//  Copyright © 2016年 JiangZhongjin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
